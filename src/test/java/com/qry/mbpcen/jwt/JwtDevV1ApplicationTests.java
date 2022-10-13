80ed9df25d25f574d0d5efcf7630eaa381d50f7e
minis24 <minis24@gmail.com>
2022-10-13 15:31:43 +0900

    최초 등록

---
+++ src/test/java/com/qry/mbpcen/jwt/JwtDevV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qry.mbpcen.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDevV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
